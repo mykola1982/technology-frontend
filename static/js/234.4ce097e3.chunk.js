3c03a89b8c9bcbf9524f4748d0da79390b2945a6
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-02-10 16:06:38 +0200

    Updates

---
+++ static/js/234.4ce097e3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktechnology=self.webpackChunktechnology||[]).push([[234],{234:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var u=t(184),c=function(){return(0,u.jsx)("div",{children:"\u0422\u0443\u0442 \u0431\u0443\u0434\u0435 \u0434\u0435\u043b\u044c\u043d\u0438\u0439 \u043e\u043f\u0438\u0441 \u043f\u0440\u043e \u0434\u0435\u0442\u0430\u043b\u044c "})}}}]);
+//# sourceMappingURL=234.4ce097e3.chunk.js.map
