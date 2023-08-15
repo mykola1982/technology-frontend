041ebb933e684fe5f0bed18fd7e0d2db99170449
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-08-15 21:52:57 +0300

    fix private route

--- src/utils/Routes/PrivateRoute.jsx
+++ src/utils/Routes/PrivateRoute.jsx
@@ -1,8 +1,32 @@
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "hooks/useAuth";
+
+// export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
+//   const { token } = useAuth();
+
+//   return !token ? <Navigate to={redirectTo} /> : Component;
+// };
+
 import { Navigate } from "react-router-dom";
 import { useAuth } from "hooks/useAuth";
+import jwt_decode from "jwt-decode";
 
 export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
   const { token } = useAuth();
 
-  return !token ? <Navigate to={redirectTo} /> : Component;
+  if (!token) {
+    return <Navigate to={redirectTo} />;
+  }
+
+  try {
+    const decodedToken = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+
+    if (decodedToken.exp < currentTime) {
+      return <Navigate to={redirectTo} />;
+    }
+    return Component;
+  } catch (error) {
+    return <Navigate to={redirectTo} />;
+  }
 };
